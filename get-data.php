290e386de6d46516f76e9067b54a01820a478599
C0wsMoo <home@local>
2024-08-17 19:30:18 +0100

    Refactored getting data

--- get-data.php
+++ get-data.php
@@ -10,14 +10,34 @@ if(!file_exists($dataFolder)){
     mkdir($dataFolder, recursive: true);
 }
 
-$urlStages = 'https://app.appmiral.com/api/v6/events/' . $eventIdentifier . '/editions/' . $editionIdentifier . '/stages?max_per_page=30';
-$urlArtists = 'https://app.appmiral.com/api/v6/events/' . $eventIdentifier . '/editions/' . $editionIdentifier . '/artists?max_per_page=300';
+$apiEndpoints = [
+    [
+        'path' => 'stages',
+        'parameters' => ['max_per_page' => 30],
+    ],
+    [
+        'path' => 'artists',
+        'parameters' => ['max_per_page' => 300],
+    ],
+];
 
-$rawResultStages = sendGetRequest($xProtect, $urlStages);
-$rawResultArtists = sendGetRequest($xProtect, $urlArtists);
+foreach ($apiEndpoints as $endpoint) {
+    echo $endpoint['path'] . PHP_EOL;
+    $base = 'https://app.appmiral.com/api/v6/events/' . $eventIdentifier . '/editions/' . $editionIdentifier . '/';
 
-file_put_contents($dataFolder . 'stages.json', $rawResultStages);
-file_put_contents($dataFolder . 'artists.json', $rawResultArtists);
+    $parameters = '';
+    if (isset($endpoint['parameters']) === true) {
+        $parameters = '?' . http_build_query($endpoint['parameters']);
+    }
+
+    $fullUrl = $base . $endpoint['path'] . $parameters;
+    $rawResult = sendGetRequest($xProtect, $fullUrl);
+
+    $saveName = $endpoint['path'];
+    if (isset($endpoint['saveName']) === true) $saveName = $endpoint['saveName'];
+
+    file_put_contents($dataFolder . $saveName . '.json', $rawResult);
+}
 
 function sendGetRequest(string $xProtect, string $endpoint) {
 
